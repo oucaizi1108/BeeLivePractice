028fc7ea0cd36e1f6a22c1bf49fd0572a5774e5c
yangxiyu1108 <oucaizi1108>
2018-04-26 15:57:01 +0800

    11

--- BeeLivePractice/AppDelegate.m
+++ BeeLivePractice/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"ppt");
+    NSLog(@"ppts");
     NSLog(@"ggg");
     NSLog(@"ggs");
     return YES;
